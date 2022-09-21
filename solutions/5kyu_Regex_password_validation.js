705b49f7157a042b43e1a45ee458ed6e44793ac4
Yanina Sebrukovich <sebrukovich_jana@mail.ru>
2022-09-22 00:30:44 +0300

    feat: add solution

--- solutions/5kyu_Regex_password_validation.js
+++ solutions/5kyu_Regex_password_validation.js
@@ -6,19 +6,4 @@
 // contains a digit
 // only contains alphanumeric characters (note that '_' is not alphanumeric)
 
-const REGEXP = /[a-z]+[A-Z]+[0-9]+/;
-
-
-console.log(REGEXP.test('fjd3IR9'))
-
-        // doTest('fjd3IR9', true);
-		// doTest('ghdfj32', false);
-		// doTest('DSJKHD23', false);
-		// doTest('dsF43', false);
-		// doTest('4fdg5Fj3', true);
-		// doTest('DHSJdhjsU', false);
-		// doTest('fjd3IR9.;', false);
-		// doTest('fjd3  IR9', false);
-		// doTest('djI38D55', true);
-		// doTest('djI3_8D55', false);
-		// doTest('djI38D55@@', false);
+const REGEXP = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9]{6,}$/;
