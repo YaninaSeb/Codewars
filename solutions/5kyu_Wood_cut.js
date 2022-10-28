c9e83ea5e7f08f6d3eeeb3c3d9c1f5ea0d083cb5
Yanina Sebrukovich <sebrukovich_jana@mail.ru>
2022-10-28 23:43:23 +0300

    fix: update solution

--- solutions/5kyu_Wood_cut.js
+++ solutions/5kyu_Wood_cut.js
@@ -11,18 +11,70 @@
 // - Result should be an integer length of small piece as long as possible;
 // Please pay attention to optimizing the code to avoid time out
 
-function woodCut(woods,n){
-    let sumWoods = woods.reduce((acc, curr) => acc += curr, 0);
-    let maxLenBlock = Math.floor(sumWoods / n);
+                //First solution
 
-    if (maxLenBlock < 1) return 0;
+// function woodCut(woods,n){
+//     let sumWoods = woods.reduce((acc, curr) => acc += curr, 0);
+//     let maxLenBlock = Math.floor(sumWoods / n);
 
-    for (; ;maxLenBlock--) {
-        let countBlocks = woods.reduce((acc, curr) =>  acc += Math.floor(curr / maxLenBlock), 0);
-        if (countBlocks >= n) break;
+//     if (maxLenBlock < 1) return 0;
+
+//     let countBlocks = 0;
+
+//     while (countBlocks < n) {
+//         countBlocks = woods.reduce((acc, curr) =>  acc += Math.floor(curr / maxLenBlock), 0);
+//         maxLenBlock -= 1;
+//     }
+
+//     return ++maxLenBlock
+// }
+
+
+
+                //Second solution
+
+function woodCut(woods,n) {
+    let maxSize = Math.max(...woods);
+
+    let binarySearch = (start, end) => {
+        const middle = Math.floor((start + (end - start)/2));
+        let countBlocs = woods.reduce((acc, curr) => acc += Math.floor(curr / middle), 0);
+
+        if (end - 1 == start) {
+            let max = woods.reduce((acc, curr) => acc += Math.floor(curr / end), 0);
+            return (n == max) ? end : start
+        }
+        if (countBlocs < n) return binarySearch(start, middle);
+        if (countBlocs >= n) return binarySearch(middle, end);
     }
 
-    return maxLenBlock
+    return binarySearch(1, maxSize);
 }
 
-console.log(woodCut([1, 1, 1], 3));
+
+                //Third solution
+
+function woodCut(woods,n) {
+    let start = 1;
+    let end = Math.max(...woods);
+
+    while (start <= end) {
+        let middle = Math.floor(start + (end - start) / 2);
+
+        if (checkLen(woods, middle, n)) {
+            start = middle + 1;
+        } else {
+            end = middle - 1;
+        }
+    }
+    return end;
+}
+
+function checkLen(arr, middle, target) {
+    let count = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        count += parseInt(arr[i] / middle)
+    }
+    return count >= target;
+}
