cf3fb2d47479d4813b20f91e0f65ee4b00e2a50b
Yanina Sebrukovich <sebrukovich_jana@mail.ru>
2022-10-31 22:53:46 +0300

    feat: add solution (for simple tests)

---
+++ solutions/6kyu_MultiFilter.js
@@ -0,0 +1,40 @@
+// Write a function called multiFilter which receives a variable number 
+// of filter functions. The function should return a function that 
+// receives one element (a compound filter function).
+
+// That is, if we have two functions:
+
+// function isEven(el){
+//   return el % 2 === 0;
+// }
+
+// function isGTten(el){
+//   return el > 10;
+// }
+
+// [1,2,3,4,10,11,12,20,21,22].filter(multiFilter(isEven, isGTten));
+// should return [12,20,22]
+
+
+//НЕ ПРОХОДИТ ВСЕ ТЕСТЫ
+
+
+let multiFilter = function(){
+  
+    if (arguments.length == 0) {
+      return function(){
+        return []
+      };
+    }
+    
+    if (arguments.length == 1) {
+      return arguments[0]
+    }
+    
+     if (arguments.length == 2) {
+       let a = arguments[0]
+       let b = arguments[1]
+       return function(a, b) {}
+    }
+    
+  };
