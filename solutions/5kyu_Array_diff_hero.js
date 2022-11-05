2a364b4fbe54d29a0231d6933107b406ec4d669b
Yanina Sebrukovich <sebrukovich_jana@mail.ru>
2022-11-05 23:29:09 +0300

    feat: add solution (failed for harder test)

---
+++ solutions/5kyu_Array_diff_hero.js
@@ -0,0 +1,7 @@
+function arrayDiffVeryFast(a, b) {
+    let res = a.filter((elem) => !b.includes(elem))
+    
+    return arrA
+}
+
+console.log(arrayDiffVeryFast([1,2,2,2,3,5,5,1,2,4,5,1], [2,1,5]))
