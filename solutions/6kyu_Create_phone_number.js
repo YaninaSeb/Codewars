19e430ccabe965da2b7332c73f09ff2738cd384b
YaninaSeb <sebrukovich_jana@mail.ru>
2022-11-16 16:39:54 +0300

    feat: add solution

---
+++ solutions/6kyu_Create_phone_number.js
@@ -0,0 +1,16 @@
+// Write a function that accepts an array of 10 integers (between 0 and 9), that 
+// returns a string of those numbers in the form of a phone number.
+
+// Example
+// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+
+// The returned format must be correct in order to complete this challenge.
+
+
+function createPhoneNumber(numbers){
+    let str = numbers.join('')
+
+    let res = str.replace(/([0-9]{3})([0-9]{3})([0-9]{4})/, '($1) $2-$3')
+
+    return res
+}
