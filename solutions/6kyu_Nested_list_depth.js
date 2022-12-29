5d8cf211cb32c94cd057cce9e5537197802242da
YaninaSeb <sebrukovich_jana@mail.ru>
2022-12-29 10:14:48 +0300

    feat: add solution

---
+++ solutions/6kyu_Nested_list_depth.js
@@ -0,0 +1,42 @@
+// A nested list (or array in JavaScript) is a list that 
+// appears as a value inside another list,
+
+// [item, item, [item, item], item]
+
+// in the above list, [item, item] is a nested list.
+
+// Your goal is to write a function that determines the depth 
+// of the deepest nested list within a given list.
+// return 1 if there are no nested lists. The list passed 
+// to your function can contain any data types.
+
+// A few examples:
+// arrayDepth([true]) // returns 1
+// arrayDepth([]) // returns 1
+// arrayDepth([2, "yes", [true, false]]) // returns 2
+// arrayDepth([1, [2, [3, [4, [5, [6], 5], 4], 3], 2], 1]) // returns 6
+// arrayDepth([2.0, [2, 0], 3.7, [3, 7], 6.7, [6, 7]]) // returns 2
+
+
+let res = [];
+
+function arrayDepth(array) {
+    let depth = 1;
+    findDepth(array, depth)
+
+	return Math.max(...res); 
+}
+
+function findDepth(arr, countDepth) {
+    let countDepthArr = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i])) {
+            let newCountDepth = countDepth + 1;
+            let newArr = arr[i];
+            countDepthArr++;
+            findDepth(newArr, newCountDepth);
+        }
+    }
+
+    if (countDepthArr == 0) res.push(countDepth);
+}
