2f874ccbd26748e4554562435ba68ac7d0287b84
YaninaSeb <sebrukovich_jana@mail.ru>
2023-04-08 23:50:06 +0300

    feat: add task

---
+++ solutions/6kyu_Word_mesh.js
@@ -0,0 +1,13 @@
+function wordMesh(arr) {
+  return arr
+    .reduce((acc, curr, ind) => {
+        if (ind > 0) {
+             
+
+        }
+        return acc;
+    }, '')
+}
+
+console.log(wordMesh(["beacon","condominium","umbilical","california"]))
+//"conumcal"
