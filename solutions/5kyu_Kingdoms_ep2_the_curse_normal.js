96ecdb403cfdf4069b410261c5d7f63de01c8046
YaninaSeb <sebrukovich_jana@mail.ru>
2023-04-20 23:33:39 +0300

    feat: add solution

--- solutions/5kyu_Kingdoms_ep2_the_curse_normal.js
+++ solutions/5kyu_Kingdoms_ep2_the_curse_normal.js
@@ -7,20 +7,18 @@ function translate(speech, vocabulary) {
         let re = new RegExp(arrWord[i].replace(/[.,?!]/g, '').replace(/[*]/g, '[a-zA-Z]'));
         let options = vocabulary.filter((item) => re.test(item));
         if (options.length === 1) {
-            res[i] = options[0];
+            res[i] = options[0].length == arrWord[i].length ? options[0] : options[0] + arrWord[i].slice(options[0].length);
             vocabulary.splice(vocabulary.indexOf(options[0]), 1);
             arrWord.splice(i, 1)
         }
+        console.log(arrWord[i], options, vocabulary, arrWord, res)
 
-        console.log(options, vocabulary, arrWord, res)
-        
-        i = (i + 1 >= arrWord.length) ? 0 : i+1; 
+        i = (i + 1 >= arrWord.length) ? 0 : i+1;
     }
 
-    // let re = new RegExp('*c*.'.replace(/[.,?!]/g, '').replace(/[*]/g, '[a-zA-Z]'));
-    // console.log(re)
-
-    //return arrWord;
+    return res.join(' ');
 }
 
-console.log(translate("a**? *c*. **e,", ["ace", "acd", "abd"]))
+console.log(translate("*** **** **s *****n, f** **e *r* m***!", ["mmy", "name", "iss", "legion", "for", "wwe", "are", "many"]))
+
+//"mmy name iss legion, for wwe are many!"],
