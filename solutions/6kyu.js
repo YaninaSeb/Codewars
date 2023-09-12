861a1fd5971926a7195f771b46f74fc9d7a47492
YaninaSeb <sebrukovich_jana@mail.ru>
2023-09-12 15:21:37 +0300

    feat: add solution

--- solutions/6kyu.js
+++ solutions/6kyu.js
@@ -31,3 +31,28 @@ function getMaxSubSum(arr) {
 
     return currSum;
 }
+
+
+
+// Сумма свойств объекта
+
+// Есть объект salaries с произвольным количеством свойств, содержащих заработные платы.
+// Напишите функцию sumSalaries(salaries), которая возвращает сумму всех зарплат с помощью 
+// метода Object.values и цикла for..of.
+// Если объект salaries пуст, то результат должен быть 0.
+
+// Например:
+
+// let salaries = {
+//   "John": 100,
+//   "Pete": 300,
+//   "Mary": 250
+// };
+
+// alert( sumSalaries(salaries) ); // 650
+
+function sumSalaries(salaries) {
+    return Object
+        .values(salaries)
+        .reduce((acc, curr) => acc + curr, 0);
+}
