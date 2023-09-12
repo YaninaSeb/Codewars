2660d9fe07a8dc850b4b71dfa88d30386bc7effa
YaninaSeb <sebrukovich_jana@mail.ru>
2023-09-12 16:04:03 +0300

    feat: add solution

--- solutions/6kyu.js
+++ solutions/6kyu.js
@@ -75,3 +75,34 @@ function sumSalaries(salaries) {
 function count(obj) {
     return Object.keys(obj).length;
 }
+
+
+
+// Максимальная зарплата
+
+// У нас есть объект salaries с зарплатами:
+
+// let salaries = {
+//   "John": 100,
+//   "Pete": 300,
+//   "Mary": 250
+// };
+// Создайте функцию topSalary(salaries), которая возвращает имя самого высокооплачиваемого сотрудника.
+
+// Если объект salaries пустой, то нужно вернуть null.
+// Если несколько высокооплачиваемых сотрудников, можно вернуть любого из них.
+// P.S. Используйте Object.entries и деструктурирование, чтобы перебрать пары ключ/значение.
+
+function topSalary(salaries) {
+    let maxSalary = null;
+    let worker = null;
+
+    for (let [name, salary] of Object.entries(salaries)) {
+        if (salary > maxSalary) {
+            maxSalary = salary;
+            worker = name;
+        }
+    }
+
+    return worker;
+}
