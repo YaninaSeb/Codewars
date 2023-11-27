9e19297765cd3d8286240f820aa85c669eae1a70
YaninaSeb <sebrukovich_jana@mail.ru>
2023-11-27 20:56:38 +0300

    feat: add solution

---
+++ solutions/6kyu_Sort_arrays_iIgnoring_case.js
@@ -0,0 +1,15 @@
+// DESCRIPTION:
+// Sort the given array of strings in alphabetical order, case insensitive. For example:
+
+// ["Hello", "there", "I'm", "fine"]  -->  ["fine", "Hello", "I'm", "there"]
+// ["C", "d", "a", "B"])              -->  ["a", "B", "C", "d"]
+
+sortme = function (names) {
+    return names.sort((a, b) => {
+        if (a.toLowerCase() > b.toLowerCase() || a.toLowerCase() === b.toLowerCase()) {
+            return 1;
+        } else {
+            return -1;
+        }
+    });
+}
